040da9cf3811d57035b03a408b1297a8f6fcfbb6
truvoice <miky@local.com>
2020-10-07 11:11:02 +0100

    adding comments

--- library/helpers/redux.js
+++ library/helpers/redux.js
@@ -21,6 +21,13 @@ export const getState = (key, defaultState) => {
     return defaultState;
 };
 
+/**
+ * Checks for additional reducers to add to the store
+ * Checks in a state config in the parent redux/reducers/(reducer_name).js config file
+ * @param key
+ * @param defaultReducers
+ * @returns {*}
+ */
 export const getReducers = (key, defaultReducers) => {
     try {
         const reducerFile = require("../../../library/redux/reducers/" + key);
