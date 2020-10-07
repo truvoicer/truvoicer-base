5f2d7ea97461d3da93d8eae472befce24930f722
truvoice <miky@local.com>
2020-10-07 17:27:29 +0100

    Testing

--- library/html-parser.js
+++ library/html-parser.js
@@ -1,6 +1,15 @@
 import React from "react";
 import {componentsConfig} from "../../config/components-config";
 
+/**
+ * Filters html data from wordpress page content
+ * Maps elements with components depending on the elements id attribute
+ * returns component if theres a match and component exists
+ *
+ * @param node
+ * @param index
+ * @returns {JSX.Element}
+ */
 export const filterHtml = (node, index) => {
     if (node.type === 'tag' && node.name === 'div') {
         if(typeof node.attribs.id !== "undefined" &&
