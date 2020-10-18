bfabf9f2e01e71949fef2183a2d3df8fff66ecdb
truvoice <miky@local.com>
2020-10-19 00:34:41 +0100

    Adding template

--- components/widgets/TextWidget.js
+++ components/widgets/TextWidget.js
@@ -2,16 +2,14 @@ import React from 'react';
 
 const TextWidget = (props) => {
     return (
-        <>
-            <div className="single-footer-widget mb-70">
-                <div className="widget-title">
+        <div className="col-xl-3 col-md-6 col-lg-3">
+            <div className="footer_widget wow fadeInUp" data-wow-duration="1.3s" data-wow-delay=".6s">
+                <h3 className="footer_title">
                     <h4>{props.data.title}</h4>
-                </div>
-                <div className="widget-content">
-                    <p>{props.data.text}</p>
-                </div>
+                </h3>
+                <p>{props.data.text}</p>
             </div>
-        </>
+        </div>
     );
 }
 
