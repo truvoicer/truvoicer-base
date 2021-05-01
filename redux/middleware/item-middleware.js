7908c0fe22694f0da0e741349608231f6b323698
truvoice <miky@local.com>
2021-05-01 14:00:30 +0100

    updates

--- redux/middleware/item-middleware.js
+++ redux/middleware/item-middleware.js
@@ -1,6 +1,11 @@
 import React from "react";
 import {getItemAction, setItemCategoryAction, setItemIdAction, setItemProviderAction} from "../actions/item-actions";
 
+/**
+ *
+ * @param requestData
+ * @returns {function(*): void}
+ */
 export function getItemMiddleware(requestData) {
     return function(dispatch) {
         return getItemAction(requestData);
